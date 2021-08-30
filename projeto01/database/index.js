46e95d12253da09795205b1c7e48f649022863e3
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-08-29 23:47:59 -0300

    fix database link

--- projeto01/database/index.js
+++ projeto01/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB_CONNECTION_ATLAS, {
+mongoose.connect(process.env.DB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
