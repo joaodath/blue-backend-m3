f1c4d695c0f5fa065aa6231240c5da345f368f0e
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-09-07 23:47:05 -0300

    project02 front working

--- projeto02/front/src/api/api.js
+++ projeto02/front/src/api/api.js
@@ -1,14 +1,14 @@
 export const Api = {
-	baseUrl: "http://localhost:3000/personagens",
+	baseUrl: "http://localhost:7000",
 
-	createUrl: () => Api.baseUrl + "/",
+	createUrl: () => Api.baseUrl + "/characters",
 
-	readAllUrl: () => Api.baseUrl + "/",
-	readSingleUrl: (id) => Api.baseUrl + "/" + id,
+	readAllUrl: () => Api.baseUrl + "/characters",
+	readSingleUrl: (id) => Api.baseUrl + "/characters/" + id,
 
-	updateUrl: (id) => Api.baseUrl + "/" + id,
+	updateUrl: (id) => Api.baseUrl + "/characters/" + id,
 
-	deleteUrl: (id) => Api.baseUrl + "/" + id,
+	deleteUrl: (id) => Api.baseUrl + "/characters/" + id,
 	deleteAllUrl: () => Api.baseUrl + "/",
 
 	// Create
