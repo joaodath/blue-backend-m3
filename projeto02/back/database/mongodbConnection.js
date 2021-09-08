d07cd7b1bb6ea1b0dc5654aebf36bb527e52286e
Joao Rodrigues <joaorodriguesdiasneto@gmail.com>
2021-09-07 21:57:01 -0300

    database connection created

---
+++ projeto02/back/database/mongodbConnection.js
@@ -0,0 +1,33 @@
+const { MongoClient, ObjectId} = require('mongodb');
+const dotenv = require('dotenv');
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_URL}`;
+const options = {useNewUrlParser: true, useUnifiedTopology: true};
+
+const client = new MongoClient(uri, options);
+
+const charactersRM = client.db(process.env.DB_NAME).collection(process.env.COLLECTION)
+
+async function dbconnect() {
+    try {
+        await client.connect( () => console.log(`Connection to MongoDB successful. Using database: ${process.env.DB_NAME}`));
+    } catch(err) {
+        console.log(`Connection failed. Error: ${err}`);
+    }
+}
+
+async function dbclose() {
+    try {
+        await client.close(() => console.log('Connection to MongoDB succesfully closed.'));
+    } catch(err) {
+        await client.close(true, () => console.log(`Connection to MongoDB forcefully closed. Error: ${err}`));
+    }
+}
+
+module.exports = {
+    client,
+    charactersRM,
+    dbconnect,
+    dbclose,
+    ObjectId
+}
